e1639b513e4017f5861bf67a6be1daf82c99b3b4
François Villemot <francois.villemot@gmail.com>
2023-07-27 18:48:41 +0200

    Better image test.

--- tests/read_image.rs
+++ tests/read_image.rs
@@ -1,6 +1,9 @@
-use image;
+use image::{GenericImageView};
 
 #[test]
 fn test_read_image(){
     let img = image::open("data/ferris.png").expect("File not found");
+    let (w, h) = img.dimensions();
+    assert_eq!(w, 880);
+    assert_eq!(h, 587);
 }
