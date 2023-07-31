a22aa7b2268d8211d893f97af155c4a1263223ad
François Villemot <francois.villemot@gmail.com>
2023-07-31 22:36:57 +0200

    Added function declarations.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use image::ImageBuffer;
+use image::Pixel;
+use ndarray::Array2;
+use std::ops::Deref;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn compute_distance_to_border<P: Pixel, Container: Deref<Target = [P::Subpixel]>>(
+    img: &ImageBuffer<P, Container>,
+) -> Array2<i32> {
+    let (w, h) = img.dimensions();
+    let mut distances = Array2::<i32>::zeros((w as usize, h as usize));
+    distances.fill(-1);
+    return distances;
 }
