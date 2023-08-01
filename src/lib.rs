63f531ecd6e1b211b7b88ab41066d7bb82c56112
François Villemot <francois.villemot@gmail.com>
2023-08-01 22:13:11 +0200

    Fixed bug in creation of distances.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
 use image::ImageBuffer;
 use image::Pixel;
 use ndarray::Array2;
-use std::fs::File;
 use std::ops::Deref;
-use std::path::Path;
 
 fn expand_distances(distances: &mut Array2<i32>, iteration: i32) -> bool {
     let mut incremented_distances = false;
@@ -15,22 +13,26 @@ fn expand_distances(distances: &mut Array2<i32>, iteration: i32) -> bool {
     let w = shape[0];
     let h = shape[1];
 
-    for x in 0..(w - 1) {
-        for y in 0..(h - 1) {
+    for x in 0..w {
+        for y in 0..h {
             let here = distances[[x as usize, y as usize]];
             if here != iteration {
                 continue;
             }
 
-            let right = distances[[(x + 1) as usize, y as usize]];
-            if right == -1 {
-                distances[[(x + 1) as usize, y as usize]] = iteration + 1;
-                incremented_distances = true;
+            if x < w - 1 {
+                let right = distances[[(x + 1) as usize, y as usize]];
+                if right == -1 {
+                    distances[[(x + 1) as usize, y as usize]] = iteration + 1;
+                    incremented_distances = true;
+                }
             }
-            let below = distances[[x as usize, (y + 1) as usize]];
-            if below == -1 {
-                distances[[x as usize, (y + 1) as usize]] = iteration + 1;
-                incremented_distances = true;
+            if y < h - 1 {
+                let below = distances[[x as usize, (y + 1) as usize]];
+                if below == -1 {
+                    distances[[x as usize, (y + 1) as usize]] = iteration + 1;
+                    incremented_distances = true;
+                }
             }
             if x > 0 {
                 let left = distances[[(x - 1) as usize, y as usize]];
@@ -63,7 +65,7 @@ pub fn compute_distance_to_border<P: Pixel, Container: Deref<Target = [P::Subpix
 
     let mut found_border = false;
     for x in 0..(w - 1) {
-        for y in 0..(h - 1) {
+        for y in 0..h {
             let pixel_here = img.get_pixel(x, y).to_rgba();
             let pixel_right = img.get_pixel(x + 1, y).to_rgba();
             if pixel_here != pixel_right {
@@ -71,11 +73,13 @@ pub fn compute_distance_to_border<P: Pixel, Container: Deref<Target = [P::Subpix
                 distances[[(x + 1) as usize, y as usize]] = 0;
                 found_border = true;
             }
-            let pixel_bottom = img.get_pixel(x, y + 1).to_rgba();
-            if pixel_here != pixel_bottom {
-                distances[[x as usize, y as usize]] = 0;
-                distances[[x as usize, (y + 1) as usize]] = 0;
-                found_border = true;
+            if y < h - 1 {
+                let pixel_bottom = img.get_pixel(x, y + 1).to_rgba();
+                if pixel_here != pixel_bottom {
+                    distances[[x as usize, y as usize]] = 0;
+                    distances[[x as usize, (y + 1) as usize]] = 0;
+                    found_border = true;
+                }
             }
         }
     }
